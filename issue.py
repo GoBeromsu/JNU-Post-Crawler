7cc2ca75de18237483f9806b834c1dd3e7a44138
goberomsu <310o@naver.com>
2022-09-10 00:48:45 +0900

    Feat : CRUD issue

---
+++ issue.py
@@ -0,0 +1,15 @@
+from github import Github
+
+token = Github(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)
+
+
+def createIssue(repo_url, content):
+    pass
+
+
+def getIssue(repo_url, content):
+    pass
+
+
+def updateIssue(repo_url, issue_enum):
+    pass
