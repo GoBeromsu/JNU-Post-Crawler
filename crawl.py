20c8921ae0b8444988426c2c51a74349bcb09bd3
goberomsu <310o@naver.com>
2022-09-11 00:18:17 +0900

    Feat : Sojoong Post

--- crawl.py
+++ crawl.py
@@ -3,6 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 sw = "https://sw.jnu.ac.kr/"
+sojoong = "https://www.sojoong.kr/www/notice/"
 
 def getHtml(url):
     html = requests.get(url)
@@ -18,4 +19,14 @@ def getSWPost(html,today):
             title=re.sub(r"\n", "", subject.text)  # 공지 제목
             link=sw + subject.find("a")["href"]  # 공지 링크
             text+=f"SW {title} {link}\n"
+    return text
+
+def getSojoongPost(html,today): 
+    text=""
+    for a in html.find_all("td","alignLeft"):
+        date = a.find("em").text
+        if(today==date):
+            title = a.find("strong","cutText").text
+            link =sojoong+"view/"+  re.compile("[0-9]{3,5}").search(a.find("a")["href"]).group()
+            text+=f"SJ {title} {link}\n"
     return text
