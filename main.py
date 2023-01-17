5027307d5a1a9307c661109aae810c8937238fc4
봄수 <310o@naver.com>
2023-01-17 20:09:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def get_credentials():
         else:
             # Read from a file or any other methods
             with open("credentials.txt", "r") as f:
-                print(f)
                 EMAIL_ADDRESS = f.readline().strip()
                 EMAIL_PASSWORD = f.readline().strip()
             return EMAIL_ADDRESS, EMAIL_PASSWORD
@@ -58,19 +57,18 @@ def check_for_new_posts(url):
         soup = BeautifulSoup(response.text, 'html.parser')
         today = datetime.now().date()
         t = soup.findAll('td',attrs={"class":"alignLeft"})
-        //추후 수정 
         post_date_tags = soup.select("#content > div.boardContainer > div > table > tbody > tr > td.alignLeft > a > span > em")
         post_title_tags = soup.select("#content > div.boardContainer > div > table > tbody > tr > td.alignLeft > a > strong")
         post_urls = soup.select("#content > div.boardContainer > div > table > tbody > tr > td.alignLeft > a")
-        
         for date_tag, title_tag,post_url in zip(post_date_tags, post_title_tags,post_urls):
-            post_title = title_tag.text
+            post_title = title_tag.text.replace(".","-")
             url = "https://www.sojoong.kr/www/notice/view/"+  re.compile("[0-9]{3,5}").search(post_url['href']).group() 
-            if date_tag.text != today:
+            if date_tag.text == today:
                 send_email("New post on website", f"Title: {post_title}\nURL: {url}")
-                break
             else:
-                print("date time is not matched")
+                # 어차피 최신순으로 불러오니까 상관 break 해도 상관 없다 
+                print(f"{today} Posted time is not matched")
+                break
     except Exception as e:
         print(f"An error occured: {e}")
 
